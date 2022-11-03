faa7851a93b6e7ee0ffb683adc402eebb17a87e2
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-11-03 18:56:20 -0300

    stupid mistake led to an error

--- concurrency-checker-core/src/main/java/concurrencytest/runtime/lock/LockBlockCause.java
+++ concurrency-checker-core/src/main/java/concurrencytest/runtime/lock/LockBlockCause.java
@@ -7,6 +7,7 @@ import concurrencytest.runtime.thread.ThreadState;
 import concurrencytest.util.CollectionUtils;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.locks.Lock;
 
 public record LockBlockCause(int resourceId, Lock lock, CheckpointDescription acquisitionPoint) implements BlockCause {
@@ -17,14 +18,14 @@ public record LockBlockCause(int resourceId, Lock lock, CheckpointDescription ac
 
     @Override
     public boolean isBlocked(ThreadState actor, RuntimeState state) {
-        if(actor.ownedResources().contains(new BlockingResource(LockType.LOCK, resourceId, Lock.class, "", 1))) {
+        if (actor.ownedResources().contains(new BlockingResource(LockType.LOCK, resourceId, Lock.class, "", 1))) {
             return false;
         }
-        if(acquisitionPoint instanceof LockAcquireCheckpointDescription desc && desc.acquireTry()) {
+        if (acquisitionPoint instanceof LockAcquireCheckpointDescription desc && desc.acquireTry()) {
             // we will allow a thread to proceed even if the resource is held by somebody else. It will fail but its ok.
             return false;
         }
-        if(lock.tryLock()) {
+        if (lock.tryLock()) {
             lock.unlock();
             return false;
         }
@@ -33,6 +34,9 @@ public record LockBlockCause(int resourceId, Lock lock, CheckpointDescription ac
 
     @Override
     public Collection<? extends ThreadState> blockedBy(RuntimeState state) {
+        if (acquisitionPoint instanceof LockAcquireCheckpointDescription desc && desc.acquireTry()) {
+            return Collections.emptyList();
+        }
         return CollectionUtils.nonNull(state.ownedResources().get(new BlockingResource(LockType.LOCK, this.resourceId, lock.getClass(), acquisitionPoint.sourceFile(), acquisitionPoint.lineNumber())));
     }
 
