8097ee187b31d23bbaaf62ca26d49da7b619fff4
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-11-20 17:31:28 -0300

    adding test for non blocking linked list

---
+++ concurrency-checker-tests/src/test/java/linkedList/LinkedListTest.java
@@ -0,0 +1,54 @@
+package linkedList;
+
+import concurrencytest.annotations.MultipleActors;
+import concurrencytest.annotations.v2.AfterActorsCompleted;
+import concurrencytest.annotations.v2.ConfigurationSource;
+import concurrencytest.config.BasicConfiguration;
+import concurrencytest.config.Configuration;
+import concurrencytest.config.ExecutionMode;
+import concurrencytest.runner.ActorSchedulerRunner;
+import linkedList.NonBlockingLinkedList.Node;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(ActorSchedulerRunner.class)
+public class LinkedListTest {
+
+    private final NonBlockingLinkedList<Integer> list = new NonBlockingLinkedList<>();
+
+    @ConfigurationSource
+    public static Configuration configuration() {
+        return new BasicConfiguration(LinkedListTest.class) {
+            @Override
+            public Collection<Class<?>> classesToInstrument() {
+                return List.of(NonBlockingLinkedList.class, Node.class);
+            }
+
+            @Override
+            public ExecutionMode executionMode() {
+                return ExecutionMode.CLASSLOADER_ISOLATION;
+            }
+
+            @Override
+            public int parallelExecutions() {
+                return 8;
+            }
+        };
+    }
+
+    @MultipleActors(numberOfActors = 2)
+    public void actor(int index) {
+        list.prepend(index);
+        Assert.assertNotNull(list.removeFirst());
+    }
+
+    @AfterActorsCompleted
+    public void listEmpty() {
+        Assert.assertEquals(0, list.size());
+    }
+
+
+}
---
+++ concurrency-checker-tests/src/test/java/linkedList/NonBlockingLinkedList.java
@@ -0,0 +1,64 @@
+package linkedList;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class NonBlockingLinkedList<T> {
+
+    public static record Node<T>(T value, AtomicReference<Node<T>> next) {
+
+        public Node(T value, Node<T> nextNode) {
+            this(value, new AtomicReference<>(nextNode));
+        }
+
+        public Node(T value) {
+            this(value, new AtomicReference<>());
+        }
+
+        public Node {
+            Objects.requireNonNull(value);
+        }
+    }
+
+    private final AtomicReference<Node<T>> head = new AtomicReference<>();
+
+    public void prepend(T t) {
+        Node<T> previousHead = head.get();
+        Node<T> nextHead = new Node<>(t, previousHead);
+        while (!head.compareAndSet(previousHead, nextHead)) {
+            previousHead = head.get();
+            nextHead = new Node<>(t, previousHead);
+        }
+    }
+
+    public T removeFirst() {
+        Node<T> previousHead = head.get();
+        if (previousHead == null) {
+            return null;
+        }
+        Node<T> nextHead = previousHead.next().get();
+        while (!head.compareAndSet(previousHead, nextHead)) {
+            previousHead = head.get();
+            if (previousHead == null) {
+                return null;
+            }
+            nextHead = previousHead.next().get();
+        }
+        return previousHead.value;
+    }
+
+
+    public int size() {
+        Node<T> previousHead = head.get();
+        if (previousHead == null) {
+            return 0;
+        }
+        int c = 0;
+        while (previousHead != null) {
+            previousHead = previousHead.next().get();
+            c++;
+        }
+        return c;
+    }
+
+}
