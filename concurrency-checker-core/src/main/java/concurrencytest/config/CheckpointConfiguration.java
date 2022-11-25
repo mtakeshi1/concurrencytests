f807d7704da9707e6df4f44bbb6e68afa4b7015d
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-11-25 12:00:32 -0300

    adding more documentation

--- concurrency-checker-core/src/main/java/concurrencytest/config/CheckpointConfiguration.java
+++ concurrency-checker-core/src/main/java/concurrencytest/config/CheckpointConfiguration.java
@@ -4,17 +4,28 @@ import concurrencytest.annotations.InjectionPoint;
 import concurrencytest.asm.ArrayElementMatcher;
 import concurrencytest.asm.BehaviourModifier;
 
-import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 
+/**
+ * Controls where the checkpoints will be injected
+ * <p>
+ * TODO this should really be on a per class basis.
+ */
 public interface CheckpointConfiguration {
 
+    /**
+     * Injection points for method calss
+     */
     default Collection<MethodInvocationMatcher> methodsToInstrument() {
         return Collections.emptyList();
     }
 
+    /**
+     * Injection points for field read / write
+     */
     default Collection<FieldAccessMatch> fieldsToInstrument() {
         return List.of(
                 (classUnderEnhancement, fieldOwner, fieldName, fieldType, accessModifier, behaviourModifiers, isFieldRead, injectionPoint) -> isFieldRead && injectionPoint == InjectionPoint.AFTER && behaviourModifiers.contains(BehaviourModifier.VOLATILE) && !behaviourModifiers.contains(BehaviourModifier.FINAL),
@@ -22,34 +33,59 @@ public interface CheckpointConfiguration {
         );
     }
 
+    /**
+     * Injection points for array elements operations
+     */
     default Collection<ArrayElementMatcher> arrayCheckpoints() {
         return List.of((classUnderEnhancement, method, injectionPoint, arrayElementRead) -> true);
     }
 
+    /**
+     * Inject default checkpoints. See {@link concurrencytest.asm.utils.SpecialMethods}
+     */
     default boolean includeStandardMethods() {
         return true;
     }
 
+    /**
+     * Whether wait / park etc should be 'enhanced'. If true, these methods will be removed and replaced by special constructs
+     */
     default boolean enhanceWaitParkNotify() {
         return true;
     }
 
+    /**
+     * If <code>true</code>, threads created within actor methods also becomes actors themselves
+     * As of now, this only works for standard {@link Thread} (and not subclasses)
+     */
     default boolean threadsEnhanced() {
         return true;
     }
 
+    /**
+     * If <code>false</code>, manual checkpoints get replaced by no-ops
+     */
     default boolean manualCheckpointsEnabled() {
         return true;
     }
 
+    /**
+     * If <code>true</code> monitor enter / exit gets special treatment and the runtime will track them for deadlocks.
+     */
     default boolean monitorCheckpointEnabled() {
         return true;
     }
 
+    /**
+     * If <code>true</code>, methods marked with 'sychronized' gets replaced by synchronized blocks.
+     */
     default boolean removeSynchronizedMethodDeclaration() {
         return true;
     }
 
+    /**
+     * If <code>true</code>, {@link Lock#lock()} and friends are also tracked for deadlocks.
+     */
     default boolean lockAcquisitionCheckpointEnabled() {
         return true;
     }
--- concurrency-checker-core/src/main/java/concurrencytest/config/CheckpointDurationConfiguration.java
+++ concurrency-checker-core/src/main/java/concurrencytest/config/CheckpointDurationConfiguration.java
@@ -2,6 +2,13 @@ package concurrencytest.config;
 
 import java.time.Duration;
 
+/**
+ * Controls the various checkpoints
+ *
+ * @param checkpointTimeout how long to wait for actors to reach checkpoints
+ * @param maxDurationPerRun maximum duration of a single execution
+ * @param maxTotalDuration maximum duration of the whole test
+ */
 public record CheckpointDurationConfiguration(Duration checkpointTimeout, Duration maxDurationPerRun, Duration maxTotalDuration) {
     public CheckpointDurationConfiguration() {
         this(Duration.ofMinutes(1), Duration.ofMinutes(10), Duration.ofHours(1));
