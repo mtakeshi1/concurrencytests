d2d847684f7c8cfb3fe43c6942650d5393f904b4
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-11-25 11:50:43 -0300

    adding more documentation

--- concurrency-checker-core/src/main/java/concurrencytest/config/ExecutionMode.java
+++ concurrency-checker-core/src/main/java/concurrencytest/config/ExecutionMode.java
@@ -1,8 +1,31 @@
 package concurrencytest.config;
 
+/**
+ * Execution mode controls how the checkpoints will be injected and the test will run.
+ */
 public enum ExecutionMode {
 
-    AUTO, FORK, RENAMING, NONE, CLASSLOADER_ISOLATION
+    /**
+     * Defaults to {@link ExecutionMode#CLASSLOADER_ISOLATION} if no classes from the JRE are in the classes to be enhanced list
+     */
+    AUTO,
+    /**
+     * Creates copy of classes with the checkpoints injected, but delegate the classloading to standard classpath.
+     * Can be used to inject checkpoints in JRE classes.
+     */
+    FORK,
+
+    /**
+     * Creates copy of classes to be enhanced with different names, loads them in a {@link concurrencytest.asm.utils.OpenClassLoader}
+     * and the tests are run in the same JVM
+     */
+    RENAMING,
+
+    /**
+     * Creates copy of classes with the checkpoints injected with the same names, but uses one {@link concurrencytest.asm.utils.SpecialClassLoader}
+     * per test thread to isolate classes. The tests are within within same JVM.
+     */
+    CLASSLOADER_ISOLATION
 
 
 }
